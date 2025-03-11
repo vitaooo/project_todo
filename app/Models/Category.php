30d653ccdf68d98c1899db64852ccf3d426ae291
victor <victorhvercosag@gmail.com>
2025-03-11 15:13:46 -0300

    criando relacionamentos no model, começando aula 9/71 do todo

--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\User;
+use App\Models\Task;
 
 class Category extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'color',
+        'user_id'
+    ];
+
+    public function user() {
+        return $this->belongsTo(User::class);
+    }
+
+    public function tasks() {
+        return $this->hasMany(Task::class);
+    }
 }
